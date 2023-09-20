338ddd8994774b8939a66606889d4422431126d0
ProjectGymLife <arthurborgess890@gmail.com>
2023-09-19 21:03:40 -0300

    teste

--- GymLife.Dominio/Entities/tetse.cs
+++ GymLife.Dominio/Entities/tetse.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace GymLife.Dominio.Entities
 {
-    internal class tetse
+    public class tetse
     {
-        sss
+        public int MyProperty { get; private set; }
     }
 }
